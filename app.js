8ba8f053b7d47a6ccb94d48cacac654a42defc5a
Vaishnavi-12345 <106369551+Vaishnavi-12345@users.noreply.github.com>
2024-03-18 14:44:13 +0530

    first commit

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ document.getElementById("play-button").addEventListener("click", function() {
   var playerName = prompt("Please enter your name:");
   if (playerName !== null && playerName.trim() !== "") {
     localStorage.setItem("playerName", playerName);
+    var backgroundMusic = document.getElementById("background-music");
+    backgroundMusic.play();
     window.location.href = "game.html";
   }
 });
